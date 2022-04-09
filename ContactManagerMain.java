724b7daada9331a0614f9dc57b2f6873fa7c7b93
Samuel Arguello <sam.rene12@gmail.com>
2022-04-08 19:37:22 -0500

    refactored initial solution to store in ContactManagerMethods.java

--- ContactManagerMain.java
+++ ContactManagerMain.java
@@ -5,21 +5,12 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
-public class ContactManagerMain {
+public class ContactManagerMain{
 
     public static void main(String[] args) {
-
-        Path contactsPath = Paths.get("data", "Contacts.txt");
-        List<String> contactsData = null;
-        
-        try {
-            if (Files.notExists(contactsPath)) Files.createDirectories(contactsPath);
-            contactsData = Files.readAllLines(contactsPath);
-        } catch(IOException e){
-            System.out.println("File or Directory could not be created.");
-            e.printStackTrace();
-        }
-
+        //instantiates ContactManagerMethods for the utility functions
+        ContactManagerMethods utilities = new ContactManagerMethods();
+        //scanner for user input
         Scanner sc = new Scanner(System.in);
         int userInput;
         do {
@@ -31,19 +22,14 @@ public class ContactManagerMain {
             System.out.println("Enter an option 1, 2, 3, 4, 5");
 
             userInput = sc.nextInt();
-            if(userInput == 1) {
-                for (String line : contactsData) {
-                    System.out.println(line);
-                }
-            }
-            else if (userInput == 2)
-                System.out.println(2);
-            else if (userInput == 3)
-            System.out.println(3);
-            else if (userInput == 4)
-                System.out.println(4);
-            else
-                System.out.println("Invalid Input");
+
+            if(userInput == 1) utilities.displayContacts();
+
+
+            else if (userInput == 2) System.out.println(2);
+            else if (userInput == 3) System.out.println(3);
+            else if (userInput == 4) System.out.println(4);
+            else System.out.println("Invalid Input");
         }
         while (userInput != 5);
 
--- ContactManagerMethods.java
+++ ContactManagerMethods.java
@@ -6,5 +6,20 @@ import java.util.List;
 
 
 public class ContactManagerMethods {
+    public Path contactsPath = Paths.get("data", "Contacts.txt");
+    List<String> contactsData = null;
 
+    protected void displayContacts(){
+
+        try {
+            if (Files.notExists(contactsPath)) Files.createDirectories(contactsPath);
+            contactsData = Files.readAllLines(contactsPath);
+        } catch(IOException e){
+            System.out.println("File or Directory could not be created.");
+            e.printStackTrace();
+        }
+        for (String line : contactsData) {
+            System.out.println(line);
+        }
+    }
 }
